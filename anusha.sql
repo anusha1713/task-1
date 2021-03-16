4bcf7ef5436484b04c9656d86dba59bab0d9f162
anusha1713 <80771659+anusha1713@users.noreply.github.com>
2021-03-16 20:56:57 +0530

    Add files via upload

---
+++ anusha.sql
@@ -0,0 +1,21 @@
+create database localdatabase2
+
+create table client(cust_name varchar(255),
+cust_id int,
+grade int,
+city varchar(255)
+)
+
+insert into client values('david','501','100','newyork')
+insert into client values('nick','601','102','london')
+insert into client values('brozen','1001','208','delhi')
+insert into client values('george','808','103','mumbai')
+
+select* from client
+
+select cust_name from client
+
+
+
+
+
